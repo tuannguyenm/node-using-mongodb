f82274f2ebcb0b86c9f660e08d62acdd7953603e
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-06-29 22:15:22 +0700

    Changing Data with CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
-import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID, updateProduct, deleteProduct } from '../controllers/controllers';
 
 const routes = (app) => {
-    app.route('/products').post(addnewProduct);
-    app.route('/products').get(getProducts);
-    app.route('/products/:ProductID').get(getProductWithID);
+    app.route('/products')
+        .post(addnewProduct)
+        .get(getProducts);
+
+    app.route('/products/:ProductID')
+        .get(getProductWithID)
+        .put(updateProduct)
+        .delete(deleteProduct);
 };
 
 export default routes;
