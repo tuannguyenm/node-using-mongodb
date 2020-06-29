5c79115aa4e64d6a58dbce4581844a8ed89a6748
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-06-29 16:40:02 +0700

    Add MongoDB & Mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
